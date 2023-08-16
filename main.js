8d4195731f38f40894e775ff9854e56238b4e108
Marcos Freire <mfreire1996@gmail.com>
2023-08-15 21:07:10 -0400

    Navbar needs work

--- main.js
+++ main.js
@@ -1,47 +1,32 @@
-const navBurger = document.getElementById("hamburger");
-const loopstudioNav = document.getElementsByClassName("nav");
-const navAnimation = document.getElementsByClassName('.active')
-const xIcon = document.getElementById("x");
+const navBurger = document.getElementById("hamburger"),
+    xIcon = document.getElementById("x"),
+    loopstudioNav = document.getElementsByClassName("nav");
 
-let navbarDisplay = () => {
-    if(window.innerWidth < 1000){
-        navBurger.style.display = "none";
-    } else {
-        navBurger.style.display = "block";
-    }
-}
+    toggleMenu.addEventListener('click',() => {
+        navBurger.classList.toggle('show')
+    });
 
-// let navbarAnimation = () => {
-//     if(navBurger === "click"){
-//         document.getElementsByClassName("active").style.display = "block";
+// const navAnimation = document.getElementsByClassName('.active')
+
+// let navbarDisplay = () => {
+//     if(window.innerWidth < 1000){
+//         navBurger.style.display = "none";
+//     } else {
+//         navBurger.style.display = "block";
 //     }
 // }
 
-navBurger.addEventListener("click", () => {
-    navBurger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-// if (active is up then make x appear on the screen. If not then keep it none)
-function navbarClose(){
-    if (navMenu.classList.contains("active")) {
-        // If the 'active' class is present, hide the menu and 'x' icon
-        navMenu.classList.remove("active");
-        xIcon.style.display = "none";
-      } else {
-        // If the 'active' class is not present, show the menu and 'x' icon
-        navMenu.classList.add("active");
-        xIcon.style.display = "block";
-      }
-}
+// navBurger.addEventListener("click", () => {
+//     navBurger.classList.toggle("active");
+//     loopstudioNav.classList.toggle("active");
+//     xIcon.classList.toggle("active");
+// })
 
 
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
-    navBurger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
 
-// let navSlider = () => {
-//     slider.style.animation = 'slider-menu 2s forwards ease-in-out';
-// }
+// xIcon.addEventListener("click", () => {
+//     navBurger.classList.remove("active");
+//     navMenu.classList.remove("active");
+//     overlay.classList.remove("active"); // Remove the overlay when the 'x' icon is clicked
+// });
